5b5a6a624c34d99f67c056abf991ea81370d4935
mbetancourtsoa <mbetancourt@soaint.com>
2026-07-15 18:06:30 -0500

    Initial commit

---
+++ src/test/java/co/com/paycash/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.paycash.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
